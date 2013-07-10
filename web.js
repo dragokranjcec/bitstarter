e29bb50e958b900c72f5bde9de681873c7a0abae
Drago Kranjcec <dragokranjcec@gmail.com>
2013-07-10 09:11:01 +0000

    Web.js with index.html ver. 2

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 
   var buffer = new Buffer(256);
-  buffer = fs.readfileSync('index.html','utf8');
+  buffer = fs.readFileSync('index.html','utf8');
   response.send(buffer.toString('utf8'));
 });
 

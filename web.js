a2056a9269008faa9c22dd2a23aaa648603de201
Drago Kranjcec <dragokranjcec@gmail.com>
2013-07-10 09:07:49 +0000

    Web.js with index.html ver. 1

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require("fs");
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+  var buffer = new Buffer(256);
+  buffer = fs.readfileSync('index.html','utf8');
+  response.send(buffer.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
